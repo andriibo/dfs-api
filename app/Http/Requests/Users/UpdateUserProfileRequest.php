40e0091e8ae9d7e750ae113e73ea2c2b5b8405fa
andrey <bondarenko.andrey@sportysystems.com>
2022-05-04 21:50:08 +0300

    feat(FS-220): PUT User Profile Endpoint

--- app/Http/Requests/Users/UpdateUserProfileRequest.php
+++ app/Http/Requests/Users/UpdateUserProfileRequest.php
@@ -9,9 +9,9 @@ class UpdateUserProfileRequest extends AbstractFormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|unique:user,' . $this->id . ',id|max:50',
-            'username' => 'required|string|unique:user,' . $this->id . ',id|between:2,50',
-            'dob' => 'required|date',
+            'email' => 'required|email|unique:user,id,' . $this->id . '|max:50',
+            'username' => 'required|string|unique:user,id' . $this->id . '|between:2,50',
+            'dob' => 'required|date_format:Y-m-d',
             'fullname' => 'required|string|between:2,50',
         ];
     }
