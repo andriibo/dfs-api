c8e7c93411eed95bc2ba21e42fc3e16801b63b4e
yurii <sklema.yurii@gmail.com>
2022-06-23 17:08:16 +0300

    feat(FS-348): add event to tests

--- app/Services/Users/UpdateBalanceService.php
+++ app/Services/Users/UpdateBalanceService.php
@@ -3,26 +3,24 @@
 namespace App\Services\Users;
 
 use App\Events\UserBalanceUpdatedEvent;
-use Exception;
+use App\Exceptions\UpdateBalanceServiceException;
 use Illuminate\Contracts\Auth\Authenticatable;
-use InvalidArgumentException;
 
 class UpdateBalanceService
 {
     /**
-     * @throws InvalidArgumentException
-     * @throws Exception
+     * @throws UpdateBalanceServiceException
      */
     public function updateBalance(Authenticatable $user, float $amount): void
     {
         if (!($user->balance + $amount) < 0) {
-            throw new InvalidArgumentException('Balance cannot be less than 0.');
+            throw new UpdateBalanceServiceException('Balance cannot be less than 0.');
         }
 
         $user->balance += $amount;
 
         if (!$user->save()) {
-            throw new Exception('Can\'t update user balance');
+            throw new UpdateBalanceServiceException('Can\'t update user balance');
         }
 
         event(new UserBalanceUpdatedEvent($user->id));
