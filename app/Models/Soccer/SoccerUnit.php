e191ac482446517c791008702012379d06ed7cae
yurii <sklema.yurii@gmail.com>
2022-07-18 18:39:27 +0300

    fix(FS-448): update position annotation

--- app/Models/Soccer/SoccerUnit.php
+++ app/Models/Soccer/SoccerUnit.php
@@ -17,7 +17,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property int                          $id
  * @property int                          $unit_id
  * @property string                       $unit_type
- * @property null|string                  $position
+ * @property string                       $position
  * @property null|string                  $salary
  * @property null|string                  $auto_salary
  * @property null|string                  $fantasy_points
