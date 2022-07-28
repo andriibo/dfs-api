0eec92e49f845f3342e988af19f05df85e711296
Andrii Bondarenko <bondarenkoa1990@gmail.com>
2022-07-28 08:01:17 +0000

    fix(FS-001): sorting

--- app/Services/UserTransactionSortFieldsService.php
+++ app/Services/UserTransactionSortFieldsService.php
@@ -7,6 +7,9 @@ class UserTransactionSortFieldsService extends SortFieldsService
     protected function getSortFields(): array
     {
         return [
+            'type' => 'type',
+            'status' => 'status',
+            'amount' => 'amount',
             'createdAt' => 'created_at',
             'updatedAt' => 'updated_at',
         ];
