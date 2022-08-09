8f3c4df233eedfb0c9b4da91a6fe38e2ce2eb6eb
Yurii <sklema.yurii@gmail.com>
2022-08-09 13:38:09 +0300

    feat(FS-432): remove unit_type from unit

--- app/Models/Soccer/SoccerUnit.php
+++ app/Models/Soccer/SoccerUnit.php
@@ -16,7 +16,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  *
  * @property int                          $id
  * @property int                          $unit_id
- * @property string                       $unit_type
  * @property null|string                  $position
  * @property null|string                  $salary
  * @property null|string                  $auto_salary
@@ -53,7 +52,6 @@ class SoccerUnit extends Model
 
     protected $fillable = [
         'unit_id',
-        'unit_type',
         'position',
         'salary',
         'auto_salary',
